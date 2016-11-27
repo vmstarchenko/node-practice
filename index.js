1e0a4280ee2fa34abd7438328c1b6b4aed6dcbf2
Aleksey Verkholantsev <alex-vee@yandex-team.ru>
2016-11-27 17:42:56 +0300

    Step 7

--- index.js
+++ index.js
@@ -2,15 +2,23 @@
 
 const PORT = process.env.PORT || 80;
 
+const cluster = require('cluster');
 const express = require('express');
 const fs = require('fs');
+const numCPUs = require('os').cpus().length;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
+if (cluster.isMaster) {
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+} else {
+    app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
 
-app.get('/file', (req, res) => {
-    const stream = fs.createReadStream('file');
+    app.get('/file', (req, res) => {
+        const stream = fs.createReadStream('file');
 
-    stream.pipe(res);
-});
+        stream.pipe(res);
+    });
+}
