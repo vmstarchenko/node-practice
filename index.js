c8e7669739e333ddc1ef1d06741f18de177fee6b
Aleksey Verkholantsev <alex-vee@yandex-team.ru>
2016-11-27 16:03:07 +0300

    Step 2

--- index.js
+++ index.js
@@ -9,21 +9,38 @@ const app = express();
 
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
 
-const fib = function (n) {
-    assert(typeof n === 'number', 'n must be a number');
+const memoize = function (fn) {
+    const cache = {};
 
-    if (n === 0) {
-        return 0;
-    }
+    return function (n) {
+        const cached = cache[n];
 
-    if (n === 1 || n === 2) {
-        return 1;
-    }
+        if (typeof cached !== 'undefined') {
+            return cached;
+        }
 
-    return fib(n - 1) + fib(n - 2);
+        const result = fn.call(this, n);
+        cache[n] = result;
+
+        return result;
+    };
 };
 
 app.get('/fib/:n', (req, res) => {
+    const fib = memoize(function (n) {
+        assert(typeof n === 'number', 'n must be a number');
+
+        if (n === 0) {
+            return 0;
+        }
+
+        if (n === 1 || n === 2) {
+            return 1;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    });
+
     const {n} = req.params;
 
     const response = String(fib(Number(n)));
