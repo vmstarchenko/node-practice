9aacdd4e12ef4b83400865d00e82a24c710f4e46
vmstarchenko <vmstarchenko@edu.hse.ru>
2016-11-30 13:35:57 +0300

    test: 4 ядра, Intel(R) Core(TM) i5-3337U CPU @ 1.80GHz

---
+++ steps/5-1/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const PORT = process.env.PORT || 8082;
+
+const assert = require('assert');
+const cluster = require('cluster');
+const express = require('express');
+
+const app = express();
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+    for (let i = 0; i < 1; i++) {
+        cluster.fork();
+    }
+} else {
+    app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
+
+    app.get('/fib/:n', (req, res) => {
+        const fib = function (n) {
+            assert(typeof n === 'number', 'n must be a number');
+
+            if (n === 0) {
+                return 0;
+            }
+
+            if (n === 1) {
+                return 1;
+            }
+
+            return fib(n - 1) + fib(n - 2);
+        };
+
+        const {n} = req.params;
+
+        const response = String(fib(Number(n)));
+
+        res.end(response);
+    });
+}
